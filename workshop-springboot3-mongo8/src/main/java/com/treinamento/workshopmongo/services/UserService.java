4f8cb5bfa45865eeb16872906a2fed679d2ba9ba
Edson Ney de Farias <edson.ney@gmail.com>
2026-08-28 17:27:15 -0300

    feat: get user by id

--- workshop-springboot3-mongo8/src/main/java/com/treinamento/workshopmongo/services/UserService.java
+++ workshop-springboot3-mongo8/src/main/java/com/treinamento/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.treinamento.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.treinamento.workshopmongo.models.dto.UserDTO;
 import com.treinamento.workshopmongo.models.entities.User;
 import com.treinamento.workshopmongo.repositories.UserRepository;
+import com.treinamento.workshopmongo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,4 +23,10 @@ public class UserService {
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
 
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+		return new UserDTO(entity);
+	}
+	
 }
