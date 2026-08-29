da39f0b2ef2c5e642749a1c0c08ab607104d828f
Edson Ney de Farias <edson.ney@gmail.com>
2026-08-28 22:10:22 -0300

    feat: get user posts

--- workshop-springboot3-mongo8/src/main/java/com/treinamento/workshopmongo/services/UserService.java
+++ workshop-springboot3-mongo8/src/main/java/com/treinamento/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.treinamento.workshopmongo.models.dto.PostDTO;
 import com.treinamento.workshopmongo.models.dto.UserDTO;
 import com.treinamento.workshopmongo.models.entities.User;
 import com.treinamento.workshopmongo.repositories.UserRepository;
@@ -31,6 +32,11 @@ public class UserService {
 	private User getEntityById(String id) {
 		Optional<User> result = repository.findById(id);
 		return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+	}	
+	
+	public List<PostDTO> getUserPosts(String id) {
+		User user = getEntityById(id);
+		return user.getPosts().stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
 	}	
 	
 	public UserDTO insert(UserDTO dto) {
