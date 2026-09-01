ea310c964696d49c463826c92eed3a8cdd355f7f
Edson Ney de Farias <edson.ney@gmail.com>
2026-09-01 10:52:36 -0300

    feat: post new department

--- workshop-springboot3-cassandra5/src/main/java/com/treinamento/workshopcassandra/controllers/DepartmentController.java
+++ workshop-springboot3-cassandra5/src/main/java/com/treinamento/workshopcassandra/controllers/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.treinamento.workshopcassandra.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.treinamento.workshopcassandra.model.dto.DepartmentDTO;
 import com.treinamento.workshopcassandra.services.DepartmentService;
 
 @RestController
 @RequestMapping(value = "/departments")
+
 public class DepartmentController {
 
 	@Autowired
@@ -30,6 +35,13 @@ public class DepartmentController {
 	public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id) {
 		DepartmentDTO obj = service.findById(id);
 		return ResponseEntity.ok(obj);
+	}	
+	
+	@PostMapping
+	public ResponseEntity<DepartmentDTO> insert(@RequestBody DepartmentDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}	
 	
 }
