9161b1bbbcf8bd5d8dd5729efd2e85c0178baa6a
Edson Ney de Farias <edson.ney@gmail.com>
2026-09-03 10:50:27 -0300

    refactor(user): remove DBRef posts relationship
    - Remove List<Post> from User entity to eliminate DBRef dependency
    - Prevent unbounded document growth in MongoDB
    - Align User model with non-blocking reactive paradigms

--- workshop-springboot3-webflux-mongo8/src/main/java/com/treinamento/workshopmongo/models/entities/User.java
+++ workshop-springboot3-webflux-mongo8/src/main/java/com/treinamento/workshopmongo/models/entities/User.java
@@ -1,11 +1,8 @@
 package com.treinamento.workshopmongo.models.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -16,9 +13,6 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
-
-	@DBRef
-	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -53,14 +47,6 @@ public class User implements Serializable {
 		this.email = email;
 	}
 	
-	public List<Post> getPosts() {
-		return posts;
-	}
-
-	public void setPosts(List<Post> posts) {
-		this.posts = posts;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
